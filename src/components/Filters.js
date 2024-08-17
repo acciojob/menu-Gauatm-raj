620f51a18fe9576e53d5039154dd938dd5390939
Gauatm-raj <gautamraj7569@gmail.com>
2024-08-17 17:00:38 +0530

    check

---
+++ src/components/Filters.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+
+const Filters = ({ categories, selectedCategory, setSelectedCategory }) => {
+
+    return <div className="categories">
+        {
+            categories.map((category,index) => {
+                    
+                return (
+                    <div className="category">
+                        <input
+                          className="input"
+                            id={`filter-btn-${index}`}
+                            data-test-id={`menu-item-${category}`}
+                            type="radio"
+                            name="category"
+                            value={category}
+                            checked={selectedCategory === category}
+                            onChange={(e) => (setSelectedCategory(e.target.value))}
+                        />
+                        <label
+                         className="label"
+                            htmlFor={`filter-btn-${index}`}>
+                            {category}
+                        </label>
+
+                    </div>
+                 )
+            })
+       }
+</div>
+
+}
+
+
+export default Filters;
