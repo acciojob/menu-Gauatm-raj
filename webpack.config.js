bcacf775321a280b54390648098294b0fe2ac4fa
Gauatm-raj <gautamraj7569@gmail.com>
2024-08-19 20:04:38 +0530

    done

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,51 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-
+const HtmlWebpackPlugin= require('html-webpack-plugin');
 module.exports = {
-  entry: "./src/index.js",
+    entry: './src/index.js',
 
-  output: {
-    path: path.join(__dirname, "/dist"),
-    filename: "index_bundle.js",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$|\.jsx$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
-      },
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: "style-loader",
-          },
-          {
-            loader: "css-loader",
-          },
+    output: {
+        path: path.join(__dirname,"/dist"),
+        filename: "index_bundle.js",
+    },
+    module:{
+        rules: [
+            {
+                test: /\.js$|\.jsx$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: 'style-loader',
+                    },
+                    {
+                        loader: 'css-loader',
+                    },
+                ]
+            },
+            {
+              test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2|otf)$/,
+              use: [
+                {
+                  loader: 'file-loader',
+                  options: {
+                    name: '[path][name].[ext]',
+                  },
+                },
+              ],
+            },
         ],
-      },
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html"
+        })
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/index.html",
-      filename: "index.html",
-    }),
-    
-  ],
-  resolve: {
-    extensions: [".js", ".jsx"],
-  },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    }
 };
